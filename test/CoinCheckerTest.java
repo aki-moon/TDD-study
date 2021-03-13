d01ee724f2660ba5f0cd7f7bb1c9d7c11e96a46c
aki.m <matsuaki4@gmail.com>
2021-03-13 23:36:22 +0900

    Coinの仕様変更に伴いテストクラスをリファクタリング

--- test/CoinCheckerTest.java
+++ test/CoinCheckerTest.java
@@ -10,26 +10,26 @@ import oopexcersise.vendingmachine.src.cointype.CoinType;
 
 class CoinCheckerTest {
 	@Test
-	void コインの重さが10gで直径が2cmの場合_コインの種類がニッケルと判定されること() {
-		Coin coin = new Coin(10, 2);
+	void コインの重さが5gで直径が21_21mmの場合_コインの種類がニッケルと判定されること() {
+		Coin coin = new Coin(5, 21.21);
 		assertEquals(CoinType.NICKEL, CoinChecker.checkCoin(coin));
 	}
 
 	@Test
-	void コインの重さが20gで直径が3cmの場合_コインの種類がダイムと判定されること() {
-		Coin coin = new Coin(20, 3);
+	void コインの重さが2_268gで直径が17_91mmの場合_コインの種類がダイムと判定されること() {
+		Coin coin = new Coin(2.268, 17.91);
 		assertEquals(CoinType.DIME, CoinChecker.checkCoin(coin));
 	}
 
 	@Test
-	void コインの重さが30gで直径が5cmの場合_コインの種類がクォーターと判定されること() {
-		Coin coin = new Coin(30, 5);
+	void コインの重さが5_67gで直径が24_26cmの場合_コインの種類がクォーターと判定されること() {
+		Coin coin = new Coin(5.67, 24.26);
 		assertEquals(CoinType.QUARTER, CoinChecker.checkCoin(coin));
 	}
 
 	@Test
-	void コインの重さが31gで直径が5cmの場合_コインの種類がUNKNOWNと判定されること() {
-		Coin coin = new Coin(31, 5);
+	void コインの重さが5_67gで直径が24_27cmの場合_コインの種類がUNKNOWNと判定されること() {
+		Coin coin = new Coin(5.67, 24.27);
 		assertEquals(CoinType.UNDEFINED, CoinChecker.checkCoin(coin));
 	}
 }
